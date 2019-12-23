3954f9a3cb5d18ea439b260331071b1eac4d09dc
sdyrdasov <sdyrdasov@agentplus.ru>
2019-12-23 14:26:09 +0400

    Refactoring

--- app/src/main/java/com/example/task1/Constants.kt
+++ app/src/main/java/com/example/task1/Constants.kt
@@ -4,8 +4,7 @@ const val DEFAULT_MASK = "# ### ## |###"
 const val POLICE_MASK = "# #### |###"
 const val TAXI_MASK = "## ### |###"
 
-const val SPECIAL_CHARS = "ABEKMHOPCTYXАВЕКМНОРСТУХabekmhopctyxавекмнорстух"
+const val CHOSEN_CHARS = "ABEKMHOPCTYXАВЕКМНОРСТУХabekmhopctyxавекмнорстух"
 const val ALL_NUMBERS = "\\d"
 
-const val REGEX_FOUND_ANY_EXCEPT = "[^$SPECIAL_CHARS$ALL_NUMBERS]"
-const val REGEX_BY_GROUPS = "[$SPECIAL_CHARS]{1,2}|$ALL_NUMBERS+"
+const val REGEX_BY_GROUPS = "[$CHOSEN_CHARS]{1,2}|$ALL_NUMBERS+"
--- app/src/main/java/com/example/task1/MaskWatcher.kt
+++ app/src/main/java/com/example/task1/MaskWatcher.kt
@@ -2,20 +2,13 @@ package com.example.task1
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.EditText
 import java.lang.Exception
-import java.util.regex.Pattern
+import java.util.*
 
 class MaskWatcher(val editText: EditText) : TextWatcher {
 
-    private val TAG = "MaskWatcher"
-
-    private val patternSymbols = Pattern.compile("[ABEKMHOPCTYXАВЕКМНОРСТУХ]")
-    private val patternNumber = Pattern.compile("[0-9]")
-
-    val regexChar = Regex("[ABEKMHOPCTYXАВЕКМНОРСТУХabekmhopctyxавекмнорстух]{1,2}|\\d+")
-    val regexNumber = Regex("([ABEKMHOPCTYXАВЕКМНОРСТУХ]{2})(\\d+)")
+    var currentMask = DEFAULT_MASK
 
     init {
         editText.addTextChangedListener(this)
@@ -29,73 +22,78 @@ class MaskWatcher(val editText: EditText) : TextWatcher {
 
     override fun afterTextChanged(inputText: Editable) {
 
-        Log.d(TAG, "afterTextChanged: [inputText: $inputText]")
-
         if (inputText.isFirstCharNumber()) {
             inputText.replace(0, 1, "")
             return
         }
-
         editText.removeTextChangedListener(this)
 
         val found = Regex(REGEX_BY_GROUPS).findAll(
             inputText.toString().replace(" ", "")
         )
-        var unMaskText = inputText.toString().replaceChars()
 
-        var block = 1
-        var currentMask = DEFAULT_MASK
-        var tempText = StringBuilder("")
-        found.forEach { f ->
-            var m = f.value
-            val idx = f.range
+        setMask(found)
+        inputText.replace(0, inputText.length, formattedByMask(formatSection(found)).toUpperCase(
+            Locale.getDefault()
+        ))
 
-            Log.d(TAG, "afterTextChanged: $m found at indexes: $idx")
+        editText.addTextChangedListener(this)
+    }
 
-            if (block == 1 && m.length == 2) {
-                currentMask = TAXI_MASK
-            } else if (block == 2) {
-                m = m.replace("[^$ALL_NUMBERS]".toRegex(), "")
-                if (m.length >= 4) {
-                    currentMask = POLICE_MASK
-                }
-            } else if (block == 3 && currentMask != DEFAULT_MASK) {
-                m = m.replace("[^$ALL_NUMBERS]".toRegex(), "")
-            } else if (block ==3 && currentMask == DEFAULT_MASK) {
-                m = m.replace("[$ALL_NUMBERS]".toRegex(), "")
-            }
-            Log.d(TAG, "afterTextChanged: [m: $m")
-            tempText.append(m)
-            block++
-        }
-        unMaskText = tempText.toString()
+    private fun formattedByMask(text: String) : String {
 
         var i = 0
-
         var maskedText = StringBuilder("")
-
-        Log.d(TAG, "afterTextChanged: [unMaskText: $unMaskText")
-
         for (char in currentMask.toCharArray()) {
-            if (char != '#' && i < unMaskText.length) {
+            if (char != '#' && i < text.length) {
                 maskedText.append(char)
                 continue
             }
             try {
-                maskedText.append(unMaskText[i])
+                maskedText.append(text[i])
             } catch (e: Exception) {
                 break
             }
             i++
         }
-        var testResult = inputText.replace(0, inputText.length, maskedText.toString().toUpperCase())
-        Log.d(TAG, "afterTextChanged: [testResult: $testResult")
-
-        editText.addTextChangedListener(this)
+        return maskedText.toString()
     }
+
+    private fun setMask(match: Sequence<MatchResult>) {
+
+        var section = 1
+        currentMask = DEFAULT_MASK
+        match.forEach { f ->
+            var m = f.value
+            if (section == 1 && m.length == 2) {
+                currentMask = TAXI_MASK
+            } else if (section == 2) {
+                m = replaceAllChars(m)
+                if (m.length >= 4) {
+                    currentMask = POLICE_MASK
+                }
+            }
+            section++
+        }
+    }
+
+    private fun formatSection(match: Sequence<MatchResult>): String {
+
+        var section = 1
+        var builder = StringBuilder("")
+
+        match.forEach { f ->
+            var m = f.value
+            if (section == 2 || (section == 3 && currentMask != DEFAULT_MASK) || section > 3) {
+                m = replaceAllChars(m)
+            }
+            builder.append(m)
+            section++
+        }
+        return builder.toString()
+    }
+
+    private fun replaceAllChars(s: String) = s.replace("[^$ALL_NUMBERS]".toRegex(), "")
 }
 
-
 fun Editable.isFirstCharNumber() = Regex("^$ALL_NUMBERS").matches(this)
-
-fun String.replaceChars() = replace(REGEX_FOUND_ANY_EXCEPT.toRegex(), "")
